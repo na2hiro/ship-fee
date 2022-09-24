e54f13210a529af41c1d6ef0ead4a03e7d0c8712
na2hiro <a@81.la>
2022-09-24 21:49:37 +0900

    Add logo

--- src/App.svelte
+++ src/App.svelte
@@ -3,18 +3,25 @@
 </script>
 
 <main>
-  <h1>メルカリ送料</h1>
+  <h1>
+    🚢
+    <wbr />
+    メルカリ送料
+  </h1>
 
   <Calculator />
 </main>
 <footer>
-  <p>不正確なデータ、機能の要望などは<a href="https://twitter.com/na2hiro" target="_blank"
-      >@na2hiro</a
-  >まで</p>
+  <p>
+    不正確なデータ、機能の要望などは<a
+      href="https://twitter.com/na2hiro"
+      target="_blank">@na2hiro</a
+    >まで
+  </p>
   <p style="margin-bottom: 0">
     2022 &copy; <a href="https://twitter.com/na2hiro" target="_blank"
       >@na2hiro</a
-  >, <a href="https://twemoji.twitter.com/" target="_blank">twemoji</a>
+    >, <a href="https://twemoji.twitter.com/" target="_blank">twemoji</a>
   </p>
 </footer>
 
@@ -22,5 +29,8 @@
   h1 {
     font-size: 3.2em;
     line-height: 1.1;
+    word-break: keep-all;
+    overflow-wrap: break-word;
+    margin-top: 0;
   }
 </style>
